ec6fd6264cfcca6cadf232e3a925e3f07928d1a5
Davidmm17 <49687969+Davidmm17@users.noreply.github.com>
2019-06-01 16:07:29 +0200

    Muestra Tablero

--- app/src/main/java/com/example/mysudokito/Casilla.java
+++ app/src/main/java/com/example/mysudokito/Casilla.java
@@ -1,6 +1,14 @@
 package com.example.mysudokito;
 
-public class Casilla {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class Casilla{
     private int numero;
 
     public Casilla() {
@@ -13,5 +21,55 @@ public class Casilla {
     public void setNumero(int numero) {
         this.numero = numero;
     }
+    /*
+    private int numero;
+
+    private Paint mPaint;
+
+    public Casilla(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mPaint = new Paint();
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        mPaint.setColor(Color.BLACK);
+
+        mPaint.setTextSize(60);
+
+        Rect bounds = new Rect();
+        mPaint.getTextBounds(String.valueOf(numero), 0,String.valueOf(numero).length(),bounds);
+
+        canvas.drawText(String.valueOf(numero),(getWidth()-bounds.width()) /2, (getHeight() + bounds.height()) /2, mPaint );
+
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+*/
+    /* private int numero;
+
+    public Casilla() {
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }*/
 }
 
--- app/src/main/java/com/example/mysudokito/Sudoku.java
+++ app/src/main/java/com/example/mysudokito/Sudoku.java
@@ -1,8 +1,16 @@
 package com.example.mysudokito;
 
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RectShape;
+import android.media.MediaExtractor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputFilter;
 import android.text.InputType;
+import android.view.Gravity;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -37,26 +45,55 @@ public class Sudoku extends AppCompatActivity {
     }
 
     public void crearTablero(){
-
+        int maxLength = 1;
+        InputFilter[] fArray = new InputFilter[1];
+        fArray[0] = new InputFilter.LengthFilter(maxLength);
         for(int x =0;x<9;x++){
             LinearLayout row = new LinearLayout(this);
             row.setOrientation(LinearLayout.HORIZONTAL);
             row.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            row.setGravity(Gravity.CENTER);
+
             for(int y =0;y<9;y++){
                 casilla = new EditText(this);
                 casilla.setInputType(InputType.TYPE_CLASS_NUMBER);
+
                 if(this.sudoku.getTablero()[x][y].getNumero()==0) casilla.setText("");
                 else{
                     casilla.setText(this.sudoku.getTablero()[x][y].getNumero()+"");
                     casilla.setEnabled(false);
+                    casilla.setTypeface(null, Typeface.BOLD);
                 }
-                casilla.setLayoutParams(new LinearLayout.LayoutParams(75, LinearLayout.LayoutParams.WRAP_CONTENT));
+                casilla.setLayoutParams(new LinearLayout.LayoutParams(111, LinearLayout.LayoutParams.WRAP_CONTENT));
+               // casilla.setBackgroundResource(R.layout.);
 
+                ShapeDrawable sd = new ShapeDrawable();
+
+                // Specify the shape of ShapeDrawable
+                sd.setShape(new RectShape());
+
+                // Specify the border color of shape
+                sd.getPaint().setColor(Color.BLACK);
+
+                // Set the border width
+                sd.getPaint().setStrokeWidth(10f);
+
+                // Specify the style is a Stroke
+                sd.getPaint().setStyle(Paint.Style.STROKE);
+
+                // Finally, add the drawable background to TextView
+                casilla.setBackground(sd);
+
+                casilla.setTextColor(Color.BLACK);
+                casilla.setGravity(Gravity.CENTER);
+                casilla.setFilters(fArray);
+                //casilla.setEnabled(false);
                 String concatenacionid = x+""+y;
                 int id = Integer.parseInt(concatenacionid);
                 casilla.setId(id);
                 row.addView(casilla);
                 this.casillasMap.put(id,casilla);
+
 
             }
             this.tablero.addView(row);
